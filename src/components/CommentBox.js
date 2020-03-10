74274ab9de4ac443d9304f45505be181aa675cc1
irvin <irvin.rodriguez@team.neustar>
2020-03-10 02:38:39 +0000

    Add Redux

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
     state = {
@@ -14,9 +15,7 @@ class CommentBox extends Component {
         // Prevent the page reload when 
         event.preventDefault();
 
-        // TODO - Call an action creator
-        // And save the comment
-
+        this.props.saveComment(this.state.comment);
         this.setState({comment: ''})
     }
 
@@ -32,4 +31,4 @@ class CommentBox extends Component {
         )}
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
